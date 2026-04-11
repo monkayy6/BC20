c77d98eccd3360b8fc1d663912c4534c9ea0f91e
monkayy6 <iannicolaspr@icloud.com>
2026-04-11 13:33:50 -0400

    LOGINN

---
+++ src/login.jsx
@@ -0,0 +1,24 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "./firebase";
+
+
+const provider = new GoogleAuthProvider();
+
+export default function Login() {
+  const handleLogin = async () => {
+    const result = await signInWithPopup(auth, provider);
+    const email = result.user.email;
+
+    if (!email || !email.endsWith("@psu.edu")) {
+      await auth.signOut();
+      alert("You must use a PSU email to sign in.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Behrend Connect</h1>
+      <button onClick={handleLogin}>Sign in with Google</button>
+    </div>
+  );
+}
